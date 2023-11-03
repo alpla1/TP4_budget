578b5248e150a943096892877cc39ad223f2196b
Alexandre Plante <alexandre-1.plante@polymtl.ca>
2023-11-03 10:48:09 -0400

    commit 13

--- myscript.py
+++ myscript.py
@@ -1,13 +1,7 @@
 import os
-import sys
 
 
-def main():
-    os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
-    os.system('git bisect run python manage.py test')
-    os.system('git bisect reset')
-
-
-if __name__ == '__main__':
-    main()
+os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
+os.system('git bisect run python manage.py test')
+os.system('git bisect reset')
 
